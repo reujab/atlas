b4288462c1850f587b6e086d3ba632a144a60030
reujab <reujab@gmail.com>
2022-03-16 23:12:59 -0400

    added gamepad.js

--- http/index.svelte
+++ http/index.svelte
@@ -2,26 +2,29 @@
 	import HomeTile from "./HomeTile";
 	import FaFilm from "svelte-icons/fa/FaFilm.svelte";
 	import FaTv from "svelte-icons/fa/FaTv.svelte";
+	import { GamepadListener } from "gamepad.js";
 
 	const hour = new Date().getHours();
 	const timeOfDay = hour >= 7 && hour <= 17 ? "day" : "night";
 	const index = Math.floor(Math.random() * (timeOfDay === "day" ? 4 : 23));
-	console.log("random number =", index);
 	const img = `url(./backgrounds/${timeOfDay}/${index}.webp)`;
 
 	const tiles = [
 		{ title: "Movies", icon: FaFilm, iconClass: "text-indigo-700" },
 		{ title: "TV Shows", icon: FaTv, iconClass: "text-lime-700" },
 	];
+	let activeTile = 0;
 
-	let activeTile = 0
-
-	function onGamepadConnect() {
-		activeTile = 1
-	}
+	const listener = new GamepadListener();
+	listener.start();
+	listener.on("gamepad:button", (e) => {
+		console.log(e);
+		if (e.detail.pressed) {
+			activeTile = Number(!activeTile);
+		}
+	});
 </script>
 
-<svelte:window on:gamepadconnected={onGamepadConnect} />
 <div id="main" class="h-screen px-48" style="background-image: {img}">
 	<div
 		class="flex flex-col gap-48 justify-center content-start flex-wrap h-full"
