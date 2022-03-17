0f813b93cf7ee57c03ac81ad2e8e5a2e543f1081
reujab <reujab@gmail.com>
2022-03-17 18:09:45 -0400

    added basic picture renderer

--- frontend/http/index.js
+++ frontend/http/index.js
@@ -1,10 +1,12 @@
 import HomeScreen from "./HomeScreen"
 import Movies from "./Movies"
+import Pictures from "./Pictures"
 import Router, { routes } from "svelte-hash-router"
 
 routes.set({
 	"/": HomeScreen,
 	"/movies": Movies,
+	"/pictures": Pictures,
 })
 
 export default new Router({
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,10 +1,30 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use rand::seq::IteratorRandom;
+use std::fs;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_image])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn get_image() -> String {
+    let mut rng = rand::thread_rng();
+    let files = fs::read_dir("/mnt").unwrap();
+    let file = files.choose(&mut rng).unwrap().unwrap();
+    let path = file.path();
+    println!("img path: {:#?}", path);
+    let bytes = fs::read(path).unwrap();
+    let mime = match infer::get(&bytes[..4]) {
+        Some(mime) => mime.mime_type(),
+        None => "video/mp4",
+    };
+    let base64 = base64::encode(bytes);
+    format!("data:{mime};base64,{base64}")
 }
