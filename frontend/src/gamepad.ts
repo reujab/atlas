a7d2488a821f540e498f7dab426ca6de66b971b0
reujab <reujab@gmail.com>
2022-08-06 21:07:25 -0400

    better support for joysticks

--- frontend/src/gamepad.ts
+++ frontend/src/gamepad.ts
@@ -1,53 +1,85 @@
 import { log } from "./log"
 
+interface Axis {
+	interval?: NodeJS.Timeout,
+	lastButton?: "up" | "down" | "left" | "right" | null,
+}
+
 const GamepadListener = require("gamepad.js").GamepadListener;
 
 const listener = new GamepadListener();
 const handlers: ((button: string) => void)[] = [];
-let timeout: NodeJS.Timeout;
+let axes: { [id: number]: Axis } = { 0: {}, 1: {} };
+let buttonInterval: NodeJS.Timeout;
 
 listener.start();
 
-listener.on("gamepad:axis", (e: any) => {
-	clearTimeout(timeout);
-	e = e.detail;
-	log("%O", e);
-	if (e.value === 0 || e.stick !== 0) {
+listener.on("gamepad:axis", ({ detail }: any) => {
+	if (detail.stick !== 0) {
 		return;
 	}
 
-	let button: string;
-	if (e.axis === 0 && e.value < 0) {
+	const axis = axes[detail.axis];
+
+	if (Math.abs(detail.value) < 0.3) {
+		clearInterval(axis.interval);
+		axis.lastButton = null;
+		return;
+	}
+
+	let button: "left" | "right" | "up" | "down";
+	if (detail.axis === 0 && detail.value < 0) {
 		button = "left";
-	} else if (e.axis === 0 && e.value > 0) {
+	} else if (detail.axis === 0 && detail.value > 0) {
 		button = "right";
-	} else if (e.axis === 1 && e.value < 0) {
+	} else if (detail.axis === 1 && detail.value < 0) {
 		button = "up";
-	} else if (e.axis === 1 && e.value > 0) {
+	} else if (detail.axis === 1 && detail.value > 0) {
 		button = "down";
-	} else {
-		log("shouldn't happen");
+	}
+
+	if (button === axis.lastButton) {
 		return;
 	}
 
-	function localDispatch() {
+	axis.lastButton = button;
+
+	clearInterval(axis.interval);
+
+	function dispatchButton() {
 		dispatch(button);
-		timeout = setTimeout(localDispatch, 250);
 	}
 
-	localDispatch();
+	dispatchButton();
+	axis.interval = setInterval(dispatchButton, 300);
 });
 
-listener.on("gamepad:button", (e: any) => {
-	if (e.detail.pressed) {
-		const button = getButtonName(e.detail.button)
-		log("%O %O", button, e.detail.button);
+listener.on("gamepad:button", ({ detail }: any) => {
+	const button = getButtonName(detail.button)
+
+	function dispatchButton() {
+		dispatch(button);
+	}
+
+	if (detail.pressed) {
+		log("%O %O", button, detail.button);
 
 		if (button === null) {
 			return;
 		}
 
-		dispatch(button);
+		dispatchButton();
+	}
+
+	switch (button) {
+		case "up":
+		case "down":
+		case "left":
+		case "right":
+			clearInterval(buttonInterval);
+			if (detail.pressed) {
+				buttonInterval = setInterval(dispatchButton, 300);
+			}
 	}
 });
 
