c415429d663bdde8c6a48473542174f7aaa6f192
reujab <reujab@gmail.com>
2023-04-20 00:10:09 -0400

    removed snap

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ set -ex
 apt-get install -y unattended-upgrades systemd-timesyncd fontconfig plymouth resolvconf snapd network-manager curl evtest
 systemctl start systemd-timesyncd
 snap install ubuntu-frame
-snap install --dangerous atlas.snap
 cp update/update.sh /usr/local/bin
 cp update/update.{service,timer} /etc/systemd/system
 
@@ -32,15 +31,6 @@ plymouth-set-default-theme -R tribar
 systemctl enable NetworkManager
 systemctl start NetworkManager
 
-# configure atlas
-snap connect atlas:alsa
-snap connect atlas:hardware-observe
-snap connect atlas:joystick
-snap connect atlas:network-manager
-snap connect atlas:process-control
-snap connect atlas:shutdown
-snap connect atlas:wayland
-
 # configure updater
 systemctl daemon-reload
 systemctl enable update.timer
@@ -50,10 +40,6 @@ systemctl start update.timer
 snap set ubuntu-frame config=vt=1
 snap set ubuntu-frame config=cursor=null
 snap set ubuntu-frame daemon=true
-
-# start atlas
-systemctl enable snap.atlas.frontend
-systemctl restart snap.atlas.frontend
 
 # disable tty1
 systemctl disable getty@tty1
--- wrapper/wrapper.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-. $SNAP/.env
-export SEEDBOX_HOST SEEDBOX_KEY AUDIO_DEVICE
-
-exec frontend
