c2861c44d87b3a212e42ebd866d9b5b4c4441a83
reujab <reujab@gmail.com>
2023-08-22 21:27:21 -0400

    prefer 720p30fps yt videos

--- overlay/src/main.rs
+++ overlay/src/main.rs
@@ -353,6 +353,7 @@ fn main() {
         &format!("--audio-device={audio_device}"),
         "--input-ipc-server=/tmp/mpv",
         "--network-timeout=300",
+        "--ytdl-format=bestvideo[height<=?720][fps<=?30][vcodec!=?vp9]+bestaudio/best",
         "--hwdec=vaapi",
         "--vo=gpu",
     ]);
