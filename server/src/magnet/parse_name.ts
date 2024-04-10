6dec8380c53b4e091a4c2fb0f45c873b01085c83
reujab <reujab@gmail.com>
2024-04-10 01:10:49 -0400

    fixed bug with duplicate seasons

--- server/src/magnet/parse_name.ts
+++ server/src/magnet/parse_name.ts
@@ -9,6 +9,8 @@ export default function parseName(name: string): ParsedName {
 	const match = name.match(episodeRegex);
 	if (!match) return { seasons: [], episode: null };
 
+	// Special case when name starts with three digits representing the season and episode. For
+	// example, 202 would represent S02E02
 	if (match[3] && match[4]) {
 		return { seasons: [Number(match[3])], episode: Number(match[4]) };
 	}
@@ -18,12 +20,17 @@ export default function parseName(name: string): ParsedName {
 		.trim()
 		.split(" ")
 		.map(Number)
-		.filter((s) => s < 256);
+		// Filter out absurd numbers and duplicate seasons
+		.filter((s, i, arr) => s < 256 && arr.indexOf(s) == i);
+
+	// If the name matches "S01-S08" or "Season 1-8", `seasons` will be `[1, 8]`.
+	// Fill in the seasons in between.
 	if (seasons.length === 2) {
 		for (let i = seasons[0] + 1; i < seasons[1]; i++) {
 			seasons.push(i);
 		}
 	}
+
 	const episode = Number(match[2]) || null;
 	return { seasons, episode };
 }
