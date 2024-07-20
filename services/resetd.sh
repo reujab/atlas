0aa0efc338b2ffce2aaa3449e4a35b4b488cc5a8
reujab <reujab@gmail.com>
2024-07-20 14:56:22 -0400

    fixed resetd

--- services/resetd.sh
+++ services/resetd.sh
@@ -2,20 +2,14 @@
 
 for input in /dev/input/event*; do
 	base=$(basename "$input")
-	if grep "Receiver Consumer Control" "/sys/class/input/$base/device/name"; then
-		break
-	fi
+	grep "Receiver Consumer Control" "/sys/class/input/$base/device/name" && break
 done
 
 while read -r event; do
-	if [[ "$event" != Event:*KEY_HOMEPAGE* ]]; then
-		continue
-	fi
-	if [[ "$event" == *"value 1"* ]]; then
-		down=$(date +%s%3N)
-	elif (($(date +%s%3N) - down >= 2000)); then
-		# systemctl restart snap.ubuntu-frame.daemon snap.atlas.frontend
-		# TODO: soft reset
-		:
+	[[ $event != Event:*KEY_HOMEPAGE* ]] && continue
+	if [[ $event == *"value 1"* ]]; then
+		down=$(date +%s)
+	elif (($(date +%s) - down > 1)); then
+		systemctl restart weston frontend
 	fi
 done < <(evtest "$input")
