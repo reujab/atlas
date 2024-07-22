e2e5a5fdd99c9a6c77623c7860bf5a5f7550e89f
reujab <reujab@gmail.com>
2024-07-22 14:00:20 -0400

    added flash.sh

---
+++ util/flash.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+if [[ ! $2 ]]; then
+	echo "Usage: $0 <img> <dev>" 1>&2
+	exit 1
+fi
+set -x
+
+dd if="$1" of="$2" bs=64K status=progress
+sgdisk -e "$2"
+sgdisk -n 0:1GiB:0 -t 0:8300 -c local "$2"
+mount "${2}p3" /mnt
+mkdir -pm700 /mnt/system-connections
+umount /mnt
+echo Success
