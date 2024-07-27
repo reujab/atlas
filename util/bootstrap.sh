1dcd18a363dfd0b7eecbe31cf52fa011a3a2d4c5
reujab <reujab@gmail.com>
2024-07-27 19:52:06 -0400

    added sgdisk

--- util/bootstrap.sh
+++ util/bootstrap.sh
@@ -78,7 +78,7 @@ install-tzupdate() {
 
 # After install-build-deps
 install-runtime-deps() {
-	apt-get install -y chrony evtest firmware-iwlwifi firmware-linux-free file \
+	apt-get install -y chrony evtest firmware-iwlwifi firmware-linux-free file gdisk \
 		fonts-{cantarell,noto{,-cjk,-extra}} libgtk-3-0 libgtk-4-1 linux-image-amd64 mpv \
 		network-manager weston yt-dlp > /dev/null
 	[[ $ATLAS_DEBUG = 1 ]] && apt-get install -y openssh-server strace > /dev/null
@@ -186,6 +186,8 @@ clean-fs() {
 	rm -rf /usr/!(bin|lib*|local|sbin|share)
 	rm -rf /usr/bin/!(bash|busybox|dbus*|evtest|file|find|hostname|journalctl|kmod|ldd|login*|mpv|nmcli|nm-online|python*|rm|run-parts|strace|su|system*|time*|udev*|weston|wpa*|yt-dlp)
 	rm -rf /usr/lib/!(NetworkManager|dbus*|file|firmware|ifupdown|locale|mime|modules|pam.d|python*|systemd|udev|*-linux-gnu)
+	rm -rf /usr/lib/systemd/system/{ModemManager,NetworkManager-,apparmor,capsule,crypt,dbus-,dpkg,fstrim,if,initrd,kmod,ldconfig,modprobe,networking,nm,pam,polkit,procps,quota,rc,seatd,system-,systemd-{ask,backlight,battery,binfmt,boot,bsod,confext,creds,firstboot,fsck-root,growfs,hostnamed,hwdb,init,kexec,localed,machine,modules,network,pcr,pstore,quota,random,remount,rfkill,storage,sys,time-wait,tpm,update,volatile},usb_modeswitch,x11}*
+	rm -rf /usr/sbin/!(NetworkManager|agetty|dhc*|fsck|getty|if*|init|ip|iucode*|mod*|pam*|reboot|sgdisk|sshd|sulogin|wpa*)
 	rm -rf /usr/lib/systemd/system/{ModemManager,NetworkManager-,apparmor,capsule,crypt,dbus-,dpkg,fstrim,if,initrd,kmod,ldconfig,modprobe,networking,nm,pam,polkit,procps,quota,rc,seatd,system-,systemd-{ask,backlight,battery,binfmt,boot,bsod,confext,creds,firstboot,fsck-root,growfs,hostnamed,hwdb,init,kexec,localed,machine,modules,network,pcr,pstore,quota,random,remount,rfkill,storage,sys,time,tpm,update,volatile},usb_modeswitch,x11}*
 	rm -rf /usr/sbin/!(NetworkManager|agetty|chronyd|dhc*|fsck|getty|if*|init|ip|iucode*|mod*|pam*|reboot|sshd|sulogin|wpa*)
 	rm -rf /usr/share/!(X11|alsa|ca-certificates|common-licenses|dbus*|dns|dri*|ffmpeg|file|font*|gl*|icons|libdrm|locale|mime|misc|pam*|sounds|systemd|vulkan|weston|zoneinfo)
